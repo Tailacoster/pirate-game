40949b9c5bc8e9ea1e161c75d8637b318687d517
Oliver Bly <oliverbly@hotmail.com>
2019-10-06 00:42:46 -0700

    Add Unity project

---
+++ Pirate Game/Assets/Scripts/PirateShip.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PirateShip : MonoBehaviour
+{
+    [Tooltip("In unity units per second")] [SerializeField] protected float accelerationSpeed = 10f;
+    [Tooltip("In unity units per second")] [SerializeField] protected float decelerationSpeed = 1f;
+    [Tooltip("In unity units per second")][SerializeField] protected float maxSpeed = 20f;
+    [SerializeField] protected float rotationSpeed = 2f;
+
+    protected float currentSpeed = 0f;
+    protected bool anchorDropped = false;
+
+    Rigidbody2D rigidBody;
+
+    protected virtual void Start()
+	{
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+	
+	protected virtual void Update()
+	{
+		
+	}
+
+    /// <summary>
+    /// Moves the ship (defaultly according to AI conditionals)
+    /// </summary>
+    protected virtual void Move()
+    {
+
+    }
+
+    /// <summary>
+    /// Calculates the speed the ship will be moving at while it is accelerating and
+    /// sets the currentSpeed accordingly
+    /// </summary>
+    protected void CalculateAcceleratingSpeed()
+    {
+        currentSpeed += accelerationSpeed * Time.deltaTime;
+        if (currentSpeed > maxSpeed)
+        {
+            currentSpeed = maxSpeed;
+        }
+    }
+
+    /// <summary>
+    /// Calculates the speed the ship will be moving at while it is decelerating and
+    /// sets the currentSpeed accordingly
+    /// </summary>
+    /// <param name="scaleFactor">Factor to scale the rate of deceleration, 1 being normal speed, 2 double, 0.5 half</param>
+    protected void CalculateDecelerationSpeed(float scaleFactor)
+    {
+        currentSpeed -= decelerationSpeed * Time.deltaTime * scaleFactor;
+    }
+}
---
+++ Pirate Game/Assets/Scripts/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : PirateShip
+{
+	protected override void Start()
+	{
+        base.Start();
+	}
+	
+	protected override void Update()
+    {
+        base.Update();
+
+        Move();
+        Rotate();
+        Anchor();
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // drop anchor
+        }
+    }
+
+    /// <summary>
+    /// Moves the player ship
+    /// </summary>
+    protected override void Move()
+    {
+        if (Input.GetKey(KeyCode.W) && !anchorDropped)
+        {
+            CalculateAcceleratingSpeed();
+        }
+        else
+        {
+            if (currentSpeed > 0 && !anchorDropped)
+            {
+                CalculateDecelerationSpeed(1f);
+            }
+            else if (currentSpeed > 0 && anchorDropped)
+            {
+                CalculateDecelerationSpeed(3f);
+            }
+            else
+            {
+                currentSpeed = 0;
+            }
+        }
+        transform.Translate(Vector2.down * currentSpeed * Time.deltaTime);
+        Debug.Log(currentSpeed);
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0, rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0, -rotationSpeed);
+        }
+    }
+
+    private void Anchor()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (anchorDropped)
+            {
+                anchorDropped = false;
+            }
+            else
+            {
+                anchorDropped = true;
+            }
+        }
+    }
+}
